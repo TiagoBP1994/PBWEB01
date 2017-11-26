4362d4e4e5f8a18a43cf8d02cc47284324797284
andreviallon <viallon.andre@gmail.com>
2017-11-26 18:26:02 +0100

    Frontend Setup + Side bar + top bar

---
+++ head.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Epico</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/TweenMax.min.js"></script>
+    <script src="js/script.js"></script>
+</head>
+
+<body>
+
+    <div class="main-container">
+                <div class="side-bar">
+
+                    <div class="logo">
+                        <img src="assets/img/logo_epico_purple.png" alt="Epico Logo">
+                    </div>
+
+                    <div class="avatar">
+                        <img src="assets/img/avatar-male.jpeg" alt="avatar img">
+                        <p class="avatar-name">Steffen Rasmussen</p>
+                    </div>
+
+                    <ul class="main-ul">
+                        <div class="menu-highlight"></div>
+                        <li class="main-li active">
+                            <span class="menu-element">Dashboard</span>
+                            <span class="menu-arrow fa fa-angle-right"></span>
+                        </li>
+                        <li class="main-li"><span class="menu-element">Browe Job</span>
+                        <span class="menu-arrow fa fa-angle-right"></span></li>
+                        <li class="main-li"><span class="menu-element">News</span>
+                        <span class="menu-arrow fa fa-angle-right"></span></li>
+                        <li class="main-li"><span class="menu-element">Account</span>
+                        <span class="menu-arrow fa fa-angle-right"></span></li>
+                    </ul>
+
+                    <div class="logout">
+                        <div class="logout-btn">Logout</div>
+                    </div>
+                    
+                </div>
+                <div class="main">
+
+
+<div class="container">
+    <div class="row">
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include 'head.php';?>
+
+<div class="col-md-2"></div>
+<div id="dashboard">    
+    <div class="header">
+        <h1 class="h1  col-md-8">Dashboard</h1>
+        <div class="contact-btn col-md-1">contact</div>
+        <div class="notification-btn col-md-1 fa fa-bell"></div>
+        
+    </div>
+    
+</div>
