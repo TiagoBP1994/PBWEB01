252c779935c2bb17895ef6c677f127b4f4c69fba
andreviallon <viallon.andre@gmail.com>
2017-12-08 07:49:13 +0100

    Revert "update signup page + beautify all files"
    This reverts commit 79f393e59d064dc3426ef3f505ef02fbf7bfb8dd.

--- header.php
+++ header.php
@@ -1,24 +1,16 @@
-<div class="side-header col-sm-2">
+ <div class="side-header col-sm-2">
     <div class="contact-btn">contact</div>
     <div class="notification-btn">
-        <div class="fa fa-bell"></div>
+        <div class="fa fa-bell"></div>        
     </div>
 </div>
 
-<div id="notifications">
-    <h3>Notifications</h3>
-    <div class="notification-container">
-        <a href="#">
-            <p class="notification">Epico has a new job for you!</p>
-        </a>
-        <a href="#">
-            <p class="notification">Epico has a new job for you!</p>
-        </a>
-        <a href="#">
-            <p class="notification">Epico has a new job for you!</p>
-        </a>
+  <div id="notifications">
+        <h3>Notifications</h3>
+        <div class="notification-container">
+            <a href="#" ><p class="notification">Epico has a new job for you!</p></a>
+            <a href="#" ><p class="notification">Epico has a new job for you!</p></a>
+            <a href="#" ><p class="notification">Epico has a new job for you!</p></a>
+        </div>
+        <div class="seeAll"><a href="#">See All</a></div>
     </div>
-    <div class="seeAll">
-        <a href="#">See All</a>
-    </div>
-</div>
